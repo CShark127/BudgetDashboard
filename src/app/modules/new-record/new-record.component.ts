f0c63cd4a7b8f325e55fda8da2e4ee2f2cbcac13
Chad Waxler <chadwaxler@gmail.com>
2020-12-26 08:58:02 -0600

    Adding logic to New Record component

--- src/app/modules/new-record/new-record.component.ts
+++ src/app/modules/new-record/new-record.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InstitutionService } from '@shared/services/institution/institution.service';
+
 @Component({
   selector: 'app-new-record',
   templateUrl: './new-record.component.html',
-  styleUrls: ['./new-record.component.scss']
+  styleUrls: ['./new-record.component.scss'],
 })
 export class NewRecordComponent implements OnInit {
+  public inst: any;
 
-  constructor() { }
+  constructor(private instService: InstitutionService) {}
 
   ngOnInit(): void {
+    this.inst = this.instService.institutionDetails;
   }
-
 }
